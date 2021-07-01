116f972747a98f06c5260f91251c4259bded0de8
IMMANUEL44 <immanuelr44@gmail.com>
2021-07-01 11:10:04 +0530

    Build:Dont build gallery2
    We dont track it...we ship prebuilt gallery pro app

--- target/product/handheld_product.mk
+++ target/product/handheld_product.mk
@@ -25,7 +25,6 @@ PRODUCT_PACKAGES += \
     Calendar \
     Contacts \
     DeskClock \
-    Gallery2 \
     OneTimeInitializer \
     preinstalled-packages-platform-handheld-product.xml \
     SettingsIntelligence \
